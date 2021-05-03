0f40e91a10a61524800e62a9177e54c9477468e5
Adam Campbell <adam@hotmeteor.com>
2021-05-02 19:14:33 -0500

    Fix middleware assignment

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,16 +3,13 @@
 namespace Hotmeteor\Inertia;
 
 use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
     public function boot()
     {
-        if (App::runningInConsole()) {
-            $this->app[Kernel::class]->appendMiddlewareToGroup('web', InertiaStatamic::class);
-        }
+        $this->app[Kernel::class]->appendMiddlewareToGroup('web', InertiaStatamic::class);
     }
 
     public function register()
